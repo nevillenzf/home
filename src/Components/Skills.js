468a860fef6588f5f6e861aa6ad8553d19202181
nevillenzf <nzng@wisc.edu>
2020-05-26 17:31:50 -0500

    Updated skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -18,21 +18,25 @@ class Skills extends React.Component {
             </div>
             <div className = "technologies">
               <div className= "column-1">
+                <div> Languages </div>
                 <div><FontAwesomeIcon icon= {faChevronRight} /> Javascript </div>
-                <div><FontAwesomeIcon icon= {faChevronRight} /> React.js </div>
-                <div><FontAwesomeIcon icon= {faChevronRight} /> Socket.io </div>
+                <div><FontAwesomeIcon icon= {faChevronRight} /> Java </div>
+                <div><FontAwesomeIcon icon= {faChevronRight} /> HTML & CSS </div>
                 <div><FontAwesomeIcon icon= {faChevronRight} /> Python </div>
-                <div><FontAwesomeIcon icon= {faChevronRight} /> Tensorflow (Keras) </div>
-                <div><FontAwesomeIcon icon= {faChevronRight} /> Spotify Web playback API </div>
+                <div><FontAwesomeIcon icon= {faChevronRight} /> C# </div>
+                <div><FontAwesomeIcon icon= {faChevronRight} /> SQL </div>
               </div>
 
               <div className = "column-2">
+                <div> Technologies </div>
+                <div><FontAwesomeIcon icon= {faChevronRight} /> Tensorflow (Keras) </div>
                 <div><FontAwesomeIcon icon= {faChevronRight} /> Node.js </div>
                 <div><FontAwesomeIcon icon= {faChevronRight} /> Express.js </div>
                 <div><FontAwesomeIcon icon= {faChevronRight} /> Flask </div>
                 <div><FontAwesomeIcon icon= {faChevronRight} /> Docker </div>
-                <div><FontAwesomeIcon icon= {faChevronRight} /> Java </div>
-                <div><FontAwesomeIcon icon= {faChevronRight} /> HTML & CSS </div>
+                <div><FontAwesomeIcon icon= {faChevronRight} /> React.js </div>
+                <div><FontAwesomeIcon icon= {faChevronRight} /> Socket.io </div>
+                <div><FontAwesomeIcon icon= {faChevronRight} /> Unity3D </div>
               </div>
             </div>
             </div>
